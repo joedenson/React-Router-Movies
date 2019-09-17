e18272a782b2f852ecf6c335f0e87632656d3fdb
Joe Denson <joedenson79@gmail.com>
2019-09-17 15:03:55 -0700

    mvp complete

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
+import MovieCard from '../Movies/MovieCard';
 
 const MovieList = props => {
   const [movies, setMovies] = useState([])
@@ -14,37 +16,28 @@ const MovieList = props => {
           console.error('Server Error', error);
         });
     }
-    
+
     getMovies();
   }, []);
-  
+
   return (
     <div className="movie-list">
-      {movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
-      ))}
-    </div>
-  );
-}
-
-function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
-  return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
+      {movies.map(movie => {
+        return (
+          <div className="save-wrapper" key={movie.id}>
+            <Link to={`/movies/${movie.id}`} className="link">
+              <MovieCard
+                key={movie.id}
+                movie={movie}
+                title={movie.title}
+                director={movie.director}
+                metascore={movie.metascore}
+                stars={movie.star}
+              />
+            </Link>
+          </div>
+        )
+      })}
     </div>
   );
 }
